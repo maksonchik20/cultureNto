9469c2c233caea29222004901334141c908dc25f
maksonchik20 <elena-g0903@mail.ru>
2023-11-15 12:51:56 +0300

    upd

--- cultureNto/cultureNto/settings.py
+++ cultureNto/cultureNto/settings.py
@@ -135,11 +135,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # MEDIA_URL = '/media/'
 # STATIC_ROOT = '/home/hostelBusiness/hostel/static'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     BASE_DIR / 'static'
-# ]
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 
 CKEDITOR_UPLOAD_PATH = "uploads/"
