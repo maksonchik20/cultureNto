a7effd86fa4dde56df4875585f4a0b4799bb7940
maksonchik20 <elena-g0903@mail.ru>
2023-12-10 11:17:15 +0300

    Исправил баг со стилями кнопки, поменял разрешения у модели день недели

--- cultureNto/main/admin/weekday.py
+++ cultureNto/main/admin/weekday.py
@@ -4,3 +4,9 @@ from import_export.admin import ImportExportMixin
 
 class WeekdayAdmin(ImportExportMixin, ModelAdmin):
     pass
+    def has_add_permission(self, request, obj=None):
+        return False
+    def has_delete_permission(self, request, obj=None):
+        return False
+    def has_change_permission(self, request, obj=None):
+        return False
